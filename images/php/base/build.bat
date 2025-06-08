a9633d19ee489967fc1dfe24e4131ec17bcb42a5
JuniYadi <juniyadi94@gmail.com>
2025-06-08 12:44:20 +0700

    Update

--- images/php/base/build.bat
+++ images/php/base/build.bat
@@ -1,13 +1,13 @@
 @echo off
 setlocal enabledelayedexpansion
 
-echo Building Docker image for Laravel PHP 8.4...
+echo Building Docker image for PHP 8.4...
 
 for /f "tokens=2 delims==" %%I in ('"wmic os get localdatetime /value"') do set datetime=%%I
 set datetime=%datetime:~2,2%%datetime:~4,2%%datetime:~6,2%%datetime:~8,2%%datetime:~10,2%
 
 REM Create the image tag
-set "image_tag=juniyadi/php:laravel-php8.4-%datetime%"
+set "image_tag=juniyadi/php:8.4-%datetime%"
 
 echo Building image with tag: %image_tag%
 echo.
